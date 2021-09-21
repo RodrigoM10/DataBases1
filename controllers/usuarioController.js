111fa573cef7e6e3f93b8faf61686f5d18cc84a5
Rodrigo <rodrigomendozar10@gmail.com>
2021-09-21 17:24:05 -0300

    password encriptada

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,23 +1,25 @@
 const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
 
 exports.crearUsuario = async (req, res) => {
-
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
   try {
     // Revisando q el email sea unico
     let usuarioEncontrado = await Usuario.findOne({ email });
 
-     
     if (usuarioEncontrado) {
-        return res.status(400).json({ msg: 'El Usuario ya existe' });
-      }
-  
+      return res.status(400).json({ msg: "El Usuario ya existe" });
+    }
 
     let usuario;
-
     //nuevo usuario
     usuario = new Usuario(req.body);
+
+    //hashear el password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
+    
     //guardar usuario
     await usuario.save();
 
